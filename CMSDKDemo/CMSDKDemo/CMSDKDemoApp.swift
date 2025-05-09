bc9bbe9723093f32b061cb37c4a6d601a35d9c19
AnyLife Air <anylife.zlb@gmail.com>
2025-05-09 21:15:43 +0800

    添加read me 说明

---
+++ CMSDKDemo/CMSDKDemo/CMSDKDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMSDKDemoApp.swift
+//  CMSDKDemo
+//
+//  Created by anylife on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CMSDKDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
